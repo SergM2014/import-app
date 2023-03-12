dbde17fd9a44032ea1a7bff7cc832d410f4b3b51
SergM2014 <weisse011@gmail.com>
2023-03-12 11:28:33 +0200

    class is cleaned up

--- app/Console/Commands/ImportProducts.php
+++ app/Console/Commands/ImportProducts.php
@@ -5,8 +5,6 @@ namespace App\Console\Commands;
 use App\Imports\ImportProduct;
 use App\Services\XlsxParser;
 use Illuminate\Console\Command;
-use Generator;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 
@@ -42,10 +40,6 @@ class ImportProducts extends Command
     {
         $start = time();
 
-        // $products = $this->xlsxParser->getDatas();
-        // $categories = $this->getCategories();
-       
-       // $this->insertProduct($products, $categories);
         $import = new ImportProduct;
         \Excel::import($import, storage_path(config('app.import.xlsxFile')));
 
@@ -55,55 +49,6 @@ class ImportProducts extends Command
         $this->info("End. The prozess lasted $time seconds");
     }
 
-    private function getCategories(): array
-    {
-        $datas = $this->xlsxParser->getCategories();
-        $categories = [];
-
-        foreach($datas as $title) {
-            $categories[$title] = DB::table('categories')->where('title', $title)->first()->id;
-        }
-       
-        return $categories;
-    }
-
-    private function insertProduct(Generator $products, array $categories): void
-    {
-        $counter = 0;
-
-        foreach ($products as $key => $product) {
-            if ($key == 0) continue;
-            $title = $this->xlsxParser->getCategoryTitle($product);
-
-            $categoryId = $categories[$title];
-
-            if($product[10] != '') {
-                unset($product[0]);
-
-                $temp = [];
-                foreach ($product as $key => $value) $temp[$key-1] = $value;
-                $product = $temp;
-            }
-
-            if (DB::table('products')->where('code', $product[5])->first()) {
-                $this->dublicatedProducts[] = $product;
-                continue;
-            }
-
-            $presenceStatus = $product[9] == config('app.import.presenceStatus')? 'yes': 'no';
-            $warrantyStatus = $product[8] == config('app.import.warrantyStatus')? 'no': (string)$product[8];
-
-            DB::table('products')->insert([
-                'category_id' => $categoryId, 'manufacturer' => $product[3], 'title' => $product[4], 'code' => $product[5],
-                'description'=> $product[6], 'price' => $product[7], 'warranty' => $warrantyStatus, 'presence' => $presenceStatus
-            ]);
-
-            $counter++;
-        }
-
-        $this->logging($counter);
-    }
-
     private function logging($counter, $dublicatedProducts): void
     {
         Log::channel('importProducts')->info("Total number of inserted in Db records is : $counter");
@@ -111,5 +56,4 @@ class ImportProducts extends Command
              Log::channel('importProducts')->info("Total number of dublicated records is : $dublicatedProducts");
         }
     }  
-    
 }
